3f4d5622a06156d9f61d02575c8ca5730530fa66
RthrBrdtt <arthur.bourdette@gmail.com>
2017-12-08 12:50:39 +0100

    Update Vandermonde.c
    Tout marche. Si vous voulez tester y'a juste à décommenter la partie apres //Testing functions

--- Vandermonde.c
+++ Vandermonde.c
@@ -4,27 +4,29 @@
 #define N 10
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-double** VandermondeY(int dim, int ordre, float xmoy){
+double** Vandermonde(int dim, int ordre, double xmoy){
 
   int j;
   int x;
   double** Vdm;
 
   
-  Vdm = calloc(dim,sizeof(int));
-    for( j = 0 ; j < ordre ; j++){
-      VdmY[j] = calloc(ordre,sizeof(int));
+  Vdm = malloc(dim * sizeof(double));
+    for( j = 0 ; j < dim ; j++){
+      Vdm[j] = calloc(ordre+1,sizeof(double));
     }
+
+    if(Vdm == NULL){printf("Erreur allocation");}
   
   for( x = 0 ; x < dim ; x++ ){
-    for( j = 0 ; j < q ; j++){
-      VdmY[x][j] = pow((x-xmoy),j);
+    for( j = 0 ; j < ordre+1 ; j++){
+      Vdm[x][j] = pow((x-xmoy),j);
     }
   }
   return Vdm;
-  
 }
-void DetruireV(float** V, int dim){
+
+void DetruireV(double** V, int dim){
   int i;
   
   for(i = 0; i < dim; i++)
@@ -32,3 +34,52 @@ void DetruireV(float** V, int dim){
   free(V);
   V=NULL;
 }
+
+//Testing functions 
+// Uncomment following function if you wish to test.
+
+/*
+void afficherVdm(double** V, int ordre, int dim){
+  int i,j;
+  
+  printf("\n");
+  for( i = 0 ; i < dim ; i++){
+    for( j = 0 ; j < ordre+1 ; j++){
+      printf(" %lf ",V[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+} 
+
+int main(){
+  int p = 2;
+  double xb = 3.5;
+  int dimx = 9;  
+  double** vdmx;
+  
+  vdmx = Vandermonde(dimx,p,xb);
+  
+  int q = 4;
+  double yb = 1.22;
+  int dimy = 6;
+  double** vdmy;
+  
+  vdmy = Vandermonde(dimy,q,yb);
+  
+  
+  afficherVdm(vdmx,p,dimx);
+  printf("\n");
+  afficherVdm(vdmy,q,dimy);
+  
+
+  DetruireV(vdmx,dimx);
+  //DetruireV(vdmy,dimy);
+
+  return 0;
+}
+*/
+
+
+///Marche Testé
+
