1018f047efe1efe774a01c97d06e41c4976d5ee4
RthrBrdtt <arthur.bourdette@gmail.com>
2017-12-08 11:37:30 +0100

    Update Vandermonde.c

--- Vandermonde.c
+++ Vandermonde.c
@@ -4,49 +4,26 @@
 #define N 10
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-double** VandermondeX(int dimX, int p, float xb){
-
-  int j;
-  float x;
-  double** VdmX;
-
-  VdmX = calloc(dimX,sizeof(int));
-    for( j = 0 ; j < p ; j++){
-      VdmX[j] = calloc(p,sizeof(int));
-    }
-  
-  for( x = 0 ; x < dimX ; x++ ){
-    for( j = 0 ; j < p ; j++){
-      VdmX[(int)x][j] = pow((x-xb),j);
-    }
-  }
-  return VdmX;
-}
-
-double** VandermondeY(int dimY, int q, float yb){
+double** VandermondeY(int dim, int ordre, float xmoy){
 
   int j;
-  float y;
-  double** VdmY;
+  int x;
+  double** Vdm;
 
   
-  VdmY = calloc(dimY,sizeof(int));
-    for( j = 0 ; j < q ; j++){
-      VdmY[j] = calloc(q,sizeof(int));
+  Vdm = calloc(dim,sizeof(int));
+    for( j = 0 ; j < ordre ; j++){
+      VdmY[j] = calloc(ordre,sizeof(int));
     }
   
-  for( y = 0 ; y < dimY ; y++ ){
+  for( x = 0 ; x < dim ; x++ ){
     for( j = 0 ; j < q ; j++){
-      VdmY[(int)y][j] = pow((y-yb),j);
+      VdmY[x][j] = pow((x-xmoy),j);
     }
   }
-  return VdmY;
+  return Vdm;
+  
 }
-
-
-
-
 void DetruireV(float** V, int dim){
   int i;
   
