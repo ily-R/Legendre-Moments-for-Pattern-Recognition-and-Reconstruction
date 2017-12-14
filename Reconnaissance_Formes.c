872100e510aacce715a090dd5b2fa3449690cefd
ilyas Aroui <arouiilyes@gmail.com>
2017-12-14 12:15:39 +0100

    Update Reconnaissance_Formes.c

--- Reconnaissance_Formes.c
+++ Reconnaissance_Formes.c
@@ -1,35 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
+#include "Reconnaissance_Formes.h"
 
-#define N 10
 // 1.A
-
-double** creerVM(int N)
-{
-    double** result;
-    int j;
-    result= malloc( (N+1) * sizeof(double*) );
-    for( j = N+1; j >0; j-- )
-        result[N+1-j] = calloc(j, sizeof(double));
-
-    return result;
-}
-
 //free la Matrix
-void freeVM(double** a) {
-
+void freeVM(double** VM)
+{
     int j;
     // Liberation
     for( j = 0; j <= N; j++ )
-        free( a[j] );
-    free( a );
+        free( VM[j] );
+    free( VM );
 }
-//Moments Géométriques
-double momentgeo(BmpImg bmpImg, int p, int q)
-{
-    //pas encore testé //structure de l'image en entrée ainsi que les coeff p et q
 
+
+//Création de la matrice Vandermonde pour les puissances d
+
+double** Vandermonde(int dim, int ordre, double xmoy){
+
+  int j;
+  int x;
+  double** Vdm;
+
+  Vdm = malloc(dim * sizeof(double*));
+    for( j = 0 ; j < dim ; j++){
+      Vdm[j] = calloc(ordre+1,sizeof(double));
+    }
+
+    if(Vdm == NULL){printf("Erreur allocation");}
+
+  for( x = 0 ; x < dim ; x++ ){
+    for( j = 0 ; j <= ordre ; j++){
+      Vdm[x][j] = pow((x-xmoy),j);
+    }
+  }
+  return Vdm;
+}
+
+
+//Moments Géométriques
+double Mgeo(BmpImg bmpImg, int p, int q)
+{
     unsigned char** image = bmpImg.img;
     int dimX, dimY; int x, y;
     double moment=0.0;
@@ -40,7 +49,6 @@ double momentgeo(BmpImg bmpImg, int p, int q)
     double ** VdmX = Vandermonde(dimX, p, 0); //pas centré donc xb = 0
     double ** VdmY = Vandermonde(dimY, q, 0); //pas centré donc yb = 0
 
-
     for (x = 0; x < dimX; x++)
     {
         for (y = 0; y < dimY; y++)
@@ -49,7 +57,6 @@ double momentgeo(BmpImg bmpImg, int p, int q)
                 moment += VdmX[x][p]*VdmY[y][q]; //no need to multiply by image[x][y] as it's a 1
         }
     }
-
     return moment;
 }
 
@@ -57,15 +64,19 @@ double momentgeo(BmpImg bmpImg, int p, int q)
 //1.B
 
 //Moments Centrés et normés
-double Mcentre(BmpImg bmpImg, int p, int q, double ohm, double xb, double yb){
+double Mcentre(BmpImg bmpImg, int p, int q){
 
     int dimX, dimY;
 
     unsigned char** image = bmpImg.img;
-    double som=0.0;
+    double somme=0.0;
     int x, y;
     dimX = bmpImg.dimX;
     dimY = bmpImg.dimY;
+
+    double ohm = Mgeo(bmpImg, 0, 0);
+    double xb = Mgeo(bmpImg, 1, 0)/ohm;
+    double yb = Mgeo(bmpImg, 0, 1)/ohm;
 
     double ** VdmX = Vandermonde(dimX, p, xb);
     double ** VdmY = Vandermonde(dimY, q, yb);
@@ -75,172 +86,169 @@ double Mcentre(BmpImg bmpImg, int p, int q, double ohm, double xb, double yb){
         for(y=0; y<dimY; y++)
         {
             if(image[x][y]!=0)
-                som += VdmX[x][p]*VdmY[y][q]/(float)(pow(ohm,(float)(p+q+2)/2));//no need to multiply by image[x][y] as it's a 1
+                somme += VdmX[x][p]*VdmY[y][q];//no need to multiply by image[x][y] as it's a 1
 
         }
     }
-    return som;
+    return somme/(float)(pow(ohm,(float)(p+q+2)/2));
 }
+
 // Creer matrice Vandermonde de moments centre
-double** creerMCentree(double** a,BmpImg bmpImg,double ohm,double** VdmX, double** VdmY)
+
+double** creerMCentree(BmpImg bmpImg,int N)
 {
+    double** VM;
+    int j;
+    VM= malloc( (N+1) * sizeof(double*) );
+    for( j = N+1; j >0; j-- )
+        VM[N+1-j] = calloc(j, sizeof(double));
+
     int p,q;
-    for(p=0;p<N+1;p++)
-        for(q=0;q<N+1-p;q++)
+    for(p=0;p<=N;p++)
+        for(q=0;q<=N-p;q++)
     {
-        a[p][q]= Mcentre(bmpImg,p,q,ohm,VdmX,VdmY);
+        VM[p][q]= Mcentre(bmpImg,p,q);
     }
-    return a;
-}
-
-//Création de la matrice Vandermonde pour les puissances d
-
-double** Vandermonde(int dim, int ordre, double xmoy){
-
-  int j;
-  int x;
-  double** Vdm;
-
-
-  Vdm = malloc(dim * sizeof(double));
-    for( j = 0 ; j < dim ; j++){
-      Vdm[j] = calloc(ordre+1,sizeof(double));
-    }
-
-    if(Vdm == NULL){printf("Erreur allocation");}
-
-  for( x = 0 ; x < dim ; x++ ){
-    for( j = 0 ; j < ordre+1 ; j++){
-      Vdm[x][j] = pow((x-xmoy),j);
-    }
-  }
-  return Vdm;
+    return VM;
 }
 
 //1.C
 
 //Calcul de Cpq
-//Calcul de Cpq
-float Cpq(int p,int q){
-    return (2*p+1)*(2*q+1)/4;
+
+double Cpq(int p,int q){
+    double result=(2*p+1)*(2*q+1)/4;
+    return result ;
 }
 
-double** creerCpq(double** a)
+double** creerCpq(int N)
 {
+    double** VM;
+    int j;
+    VM= malloc( (N+1) * sizeof(double*) );
+    for( j = N+1; j >0; j-- )
+        VM[N+1-j] = calloc(j, sizeof(double));
+
     int p,q;
-     for(p=0;p<N+1;p++)
-        for(q=0;q<N+1-p;q++)
+     for(p=0;p<=N;p++)
+        for(q=0;q<=N-p;q++)
     {
-        a[p][q]= Cpq(int p,int q);
+        VM[p][q]= Cpq(int p,int q);
     }
-    return a
+    return VM;
 }
 
 //Moments de Legendre
 
 //Matrice triangulaire des coefficients de Legendre
 //Renvoie la matrice triangulaire, en fonction de l'ordre NLeg défini au début du main
-double** coeffLeg () {
+
+double** coeffLeg (int Nleg) {
 
     //N = taille de la matrice
-
     int i, j;
     float n = 1.0; //parcours des coeff
 
     // Declaration
-    double ** a; //matrice triangulaire coeff Legendre
+    double ** coeff; //matrice triangulaire coeff Legendre
 
     // Allocation dynamique
-    a = malloc( (NLeg+1) * sizeof(double*) ); // de la colonne for( int i = 0; i < N; i++ )
+    coeff = malloc( (NLeg+1) * sizeof(double*) ); // de la colonne for( int i = 0; i < N; i++ )
     for( int j = 0; j <= NLeg; j++ )
-        a[j] = calloc( j+1, sizeof(double) ); // Tableau 1D de  → i+1 elements
+        coeff[j] = calloc( j+1, sizeof(double) ); // Tableau 1D de  → i+1 elements
 
     //Conditions initiales
 
-    a[0][0] = 1.0; a[1][0] = 0.0; a[1][1]= 1.0;
+    coeff[0][0] = 1.0; coeff[1][0] = 0.0; coeff[1][1]= 1.0;
 
     for(j = 2; j<= NLeg; j++){ //Entre la 2e ligne et la Nème ligne
 
         for(i=0; i<= n+1; i++){ //Parcours de a entre 0 et n+1
 
             if (i == 0){  //Condition sur la première colonne
-                a[j][i] = (-n/(n+1))*a[(int)n-1][0];
-                //printf("n = %f; a[%d][%d] : %lf \n", n, j, i, a[j][i]);
+                coeff[j][i] = (-n/(n+1))*coeff[(int)n-1][0];
+                //printf("n = %f; coeff[%d][%d] : %lf \n", n, j, i, coeff[j][i]);
             }
 
             else if (i == n+1) //Condition sur la dernière diagonale
-                a[j][i] = ((2*n+1)/(n+1))*a[(int)n][(int)n];
+                coeff[j][i] = ((2*n+1)/(n+1))*coeff[(int)n][(int)n];
 
             else if (i == n) //Condition sur l'avant dernière diagonale
-                a[j][i] = 0;
+                coeff[j][i] = 0;
 
             else //Autres
-                a[j][i] = ((2*n+1)/(n+1))*a[(int)n][i-1] - (n/(n+1))*a[(int)n-1][i];
+                coeff[j][i] = ((2*n+1)/(n+1))*coeff[(int)n][i-1] - (n/(n+1))*coeff[(int)n-1][i];
         }
         n++; //Incrémentation de n
     }
-
-
-    // Affichage: attention aux tailles des tableaux
+    /*/ Affichage: attention aux tailles des tableaux
     for( j = 0; j <= NLeg; j++ ) {
         printf( "N = %d : ", j);
         for( i = 0; i <= j; i++ )
-            printf( "%lf  ", a[j][i] ); printf( "\n" );
-    }
+            printf( "%lf  ", coeff[j][i] ); printf( "\n" );
+    }*/
 
-
-    return a;
+    return coeff;
 }
 
 //Moments de Legendre
 
-double lambda(int p, int q, double Cpq, double** coeff, double** Mcentree)
+double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre)
 {
     int i,j;
     double result=0;
     for(i=0;i<p;i++)
         for(j=0;j<q;j++)
-        result+= coeff[p][i]*coeff[q][j]*Mcentree[i][j];
+        result+= coeff[p][i]*coeff[q][j]*Mcentre[i][j];
 
     return Cpq*result;
 }
 
-double** creerLambda(double** a,double** Cpq,double** coeff,double** Mcentre)
+double** creerMlegendre(int N,double** Cpq,double** coeff,double** Mcentre)
 {
+    double** VM;
+    int j;
+    VM= malloc( (N+1) * sizeof(double*) );
+    for( j = N+1; j >0; j-- )
+        VM[N+1-j] = calloc(j, sizeof(double));
+
     int p,q;
     for(p=0;p<N+1;p++)
         for(q=0;q<N+1-p;q++)
     {
-        a[p][q]= lambda(p,q,Cpq[p][q],coeff,Mcentree);
+        VM[p][q]= Mlegendre(p,q,Cpq[p][q],coeff,Mcentre);
     }
-    return a;
+    return VM;
 }
 
 //Construction des polynômes de Legendre
-double polyLeg(int x, int n, double** a){
+
+double polyLeg(int x, int n, double** coeff){
 
     int i; double sum = 0;
     for(i=0; i<=n; i++){
-        sum += a[n][i]*pow(x, i);
+        sum += coeff[n][i]*pow(x, i);
     }
     return sum;
 }
+
 //1.D
 //Reconstruction de l'image à partir des moments de Legendre
-double ImageMomLeg(int x, int y,double** lambda,double** a)
+
+double ImageMomLeg(int x, int y,double** Mlegendre,double** coeff)
 {
     int p, q;
     double pixel = 0;
     for (p=0; p <= N; p++)
         for (q = 0; q <= N - p; q++)
 
-            pixel += lambda[p][q]*polyLeg(x,n,a)*polyLeg(y,n,a);
+            pixel += Mlegendre[p][q]*polyLeg(x,n,coeff)*polyLeg(y,n,coeff);
 
 
     return pixel;
 }
 
-double** imageReconstruite(BmpImg bmpimg,double** lambda, double** a)
+double** imageReconstruite(BmpImg bmpimg,double** Mlegendre, double** coeff)
 {
     int i,x,y;
     int dimX=bmpimg.dimX;
@@ -249,22 +257,24 @@ double** imageReconstruite(BmpImg bmpimg,double** lambda, double** a)
     image= malloc(dimX,sizeof(double*));
     for(i=0;i<dimX;i++)
     {
-        image[i]=calloc(dimY*sizeof(double))
+        image[i]=calloc(dimY*sizeof(double));
     }
     for(x=0;x<dimX,x++)
         for(y=0;y<dimY;y++)
     {
-        image[x][y]=ImageMomLeg(x,y,lambda,a);
+        image[x][y]=ImageMomLeg(x,y,Mlegendre,coeff);
     }
+    return image;
 }
-double distanceEuclidienne(double** lambda1, double** lambda2)
+
+double distanceEuclidienne(double** Mlegendre1, double** Mlegendre2)
 {
     int p,q;
     double somme=0;
     for(p=0;p<=N;p++)
         for(q=0;q<=N-p;q++)
     {
-        somme+=(lambda1[p][q]-lambda2[p][q])*(lambda1[p][q]-lambda2[p][q])
+        somme+=(Mlegendre1[p][q]-Mlegendre2[p][q])*(Mlegendre1[p][q]-Mlegendre2[p][q])
     }
 
    return sqrt(somme);
