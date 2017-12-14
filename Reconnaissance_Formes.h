1d877fad4c5c2e08dc1d52daf7120bcac215bb4a
ilyas Aroui <arouiilyes@gmail.com>
2017-12-14 12:15:04 +0100

    Update Reconnaissance_Formes.h

--- Reconnaissance_Formes.h
+++ Reconnaissance_Formes.h
@@ -2,7 +2,7 @@
 //  Reconnaissance_Formes.h
 //  Projet_3E103
 //
-//  Created by Baldet Ryo on 08/12/2017.
+//  Created by Baldet Ryo and ilyas Aroui on 14/12/2017.
 //  Copyright © 2017 Team_Projet. All rights reserved.
 //
 
@@ -15,47 +15,46 @@
 
 
 // 1.A
-
-//Moments Géométriques
-double momentgeo(BmpImg bmpImg, int p, int q);
-
-
-//1.B
-
-//Moments Centrés et normés
-double Mcentre(BmpImg bmpImg, int p, int q, double ohm, double** VdmX, double** VdmY);
+//free la Matrix
+void freeVM(double** VM);
 
 //Création de la matrice Vandermonde pour les puissances d
-double** Vandermonde(int dim, int p, float xmoy);
+double** Vandermonde(int dim, int ordre, double xmoy);
 
-//Destruction des matrices de Vandermonde
-void DetruireV(float** V, int dim);
+//Moments Géométriques
+double Mgeo(BmpImg bmpImg, int p, int q);
 
+//1.B
+//Moments Centrés et normés
+double Mcentre(BmpImg bmpImg, int p, int q);
+
+// Creer matrice Vandermonde de moments centre
+double** creerMCentree(BmpImg bmpImg,int N);
 
 //1.C
-
 //Calcul de Cpq
-float cpq(int p,int q);
-    
-//Moments de Legendre
-double lambda(BmpImg bmpImg, int p, int q, double xb, double yb, float c, float**a);
+double Cpq(int p,int q);
 
+double** creerCpq(int N);
+
+//Moments de Legendre
 //Matrice triangulaire des coefficients de Legendre
-double** coeffLeg (); //Renvoie la matrice triangulaire, en fonction de l'ordre NLeg défini au début du main
-    
-//Destruction de la matrice
-void freeLeg(double** a);
+//Renvoie la matrice triangulaire, en fonction de l'ordre NLeg défini au début du main
+double** coeffLeg (int Nleg);
+
+//Moments de Legendre
+double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre);
 
 //Construction des polynômes de Legendre
-double polyLeg(int x, int n, double** a);
 
-
+double polyLeg(int x, int n, double** coeff);
 //1.D
-
 //Reconstruction de l'image à partir des moments de Legendre
-double** ImageMomLeg(int x, int y, double** a);
 
+double ImageMomLeg(int x, int y,double** Mlegendre,double** coeff);
 
+double** imageReconstruite(BmpImg bmpimg,double** Mlegendre, double** coeff);
 
+double distanceEuclidienne(double** Mlegendre1, double** Mlegendre2);
 
 #endif /* Reconnaissance_Formes_h */
