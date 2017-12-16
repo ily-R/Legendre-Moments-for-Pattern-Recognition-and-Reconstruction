d029497ba8074fe6100570e0c50cb3f1e720d655
RYO <34000759+rrryyyooo@users.noreply.github.com>
2017-12-16 21:47:04 +0100

    Final header (hope so)

--- Reconnaissance_Formes.h
+++ Reconnaissance_Formes.h
@@ -6,26 +6,30 @@
 //  Copyright © 2017 Team_Projet. All rights reserved.
 //
 
-#ifndef Reconnaissance_Formes_h
-#define Reconnaissance_Formes_h
+#ifndef RECONNAISSANCE_FORMES_H_INCLUDED
+#define RECONNAISSANCE_FORMES_H_INCLUDED
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
+#include "myBmpGris.h"
+
+#define N_ordre 10
+
 
 // 1.A
 //free la Matrix
-void freeVM(double** VM);
+void freeVM(int N, double** VM);
 
-//Création de la matrice Vandermonde pour les puissances d
+//CrÈation de la matrice Vandermonde pour les puissances d
 double** Vandermonde(int dim, int ordre, double xmoy);
 
-//Moments Géométriques
+//Moments GÈomÈtriques
 double Mgeo(BmpImg bmpImg, int p, int q);
 
 //1.B
-//Moments Centrés et normés
+//Moments CentrÈs et normÈs
 double Mcentre(BmpImg bmpImg, int p, int q);
 
 // Creer matrice Vandermonde de moments centre
@@ -39,22 +43,22 @@ double** creerCpq(int N);
 
 //Moments de Legendre
 //Matrice triangulaire des coefficients de Legendre
-//Renvoie la matrice triangulaire, en fonction de l'ordre NLeg défini au début du main
+//Renvoie la matrice triangulaire, en fonction de l'ordre NLeg dÈfini au dÈbut du main
 double** coeffLeg (int Nleg);
 
 //Moments de Legendre
 double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre);
 
-//Construction des polynômes de Legendre
+//Construction des polynÙmes de Legendre
 
 double polyLeg(int x, int n, double** coeff);
 //1.D
-//Reconstruction de l'image à partir des moments de Legendre
+//Reconstruction de l'image ‡ partir des moments de Legendre
 
-double ImageMomLeg(int x, int y,double** Mlegendre,double** coeff);
+double ImageMomLeg(int N, int x, int y,double** Mlegendre,double** coeff);
 
-double** imageReconstruite(BmpImg bmpimg,double** Mlegendre, double** coeff);
+double** imageReconstruite(int N, BmpImg bmpimg,double** Mlegendre, double** coeff);
 
-double distanceEuclidienne(double** Mlegendre1, double** Mlegendre2);
+double distanceEuclidienne(int N, double** Mlegendre1, double** Mlegendre2);
 
-#endif /* Reconnaissance_Formes_h */
+#endif // RECONNAISSANCE_FORMES_H_INCLUDED
