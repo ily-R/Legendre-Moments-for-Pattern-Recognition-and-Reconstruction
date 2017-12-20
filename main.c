a72786692373f98bbfcb8a12ea173367b7fb243f
ilyas Aroui <arouiilyes@gmail.com>
2017-12-20 18:12:08 +0100

    Update main.c
    Final

--- main.c
+++ main.c
@@ -1,40 +1,48 @@
-//
-//  main.c
-//  Projet_3E103
-//
-//  Created by Baldet Ryo on 07/12/2017.
-//  Copyright © 2017 Team_Projet. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-#define NLeg 50
-#define p
-#define q
+#include "myBmpGris.h"
+#include "Reconnaissance_Formes.h"
 
+#define ordre 45
+int main()
+{
+    int i,j;
+    BmpImg bmpImg = readBmpImage("lady.bmp");
+    double** Mcentree= MatMCentree(bmpImg,ordre,2);
 
-int main(int argc, const char * argv[]) {
-    
-    
-    double ohm = momentgeo(BmpImg, 0, 0);
-    double xb = momentgeo(BmpImg, 1, 0)/ohm;
-    double yb = momentgeo(BmpImg, 0, 1)/ohm;
-    
-        
-    double Nabla = Mcentre(BmpImg bmpImg, int p, int q, double** VdmX, double** VdmY);
-    //Calcul moments centrÈs normÈs
-    
-    float cpq = cpq(int p,int q);
-    double Lambda = lambda(BmpImg bmpImg, int p, int q, double xb, double yb, cpq, float**a);
-    
-    
-    
-    
-    
-    void DetruireV(float** V, int dim){
+    double** Cpq= MatCpq(ordre);
 
+    double** coeff=  MatCoeffLeg(ordre);
+
+    double** MatLegendre= MatMlegendre(ordre,Cpq,coeff,Mcentree);
+
+    //Calcul de la matrice des moments de Legendre (vérifié et comparé avec le fichier de Dietenbeck)
+
+    //Reconstruction de l'image à partir des moments (à ne pas faire parce que le temps de compilation est énorme
+    //                                                et les résultats ne donnent rien).
+
+//    double** image= imageReconstruite(ordre,bmpImg,MatLegendre,coeff);
+
+    //Affichage de la matrice des moments de Legendre, ou de n'importe laquelle en remplacant par le nom correspondant
+
+    for(i=0;i<=ordre;i++)
+    {
+        for(j=0;j<=ordre-i;j++)
+        {
+            printf("%lf ",MatLegendre[i][j]);
+        }
+        printf("\n");
+    }
+
+    //Libération des matrices et de la structure image
+
+    freeVM(ordre,Mcentree);
+    freeVM(ordre,Cpq);
+    freeVM(ordre,coeff);
+    freeVM(ordre,MatLegendre);
+//    freeVM(ordre,image);
+    freeBmpImg(&bmpImg);
 
     return 0;
 }
