35411957ca64278e3eab623bf664c4074a306195
ilyas Aroui <arouiilyes@gmail.com>
2017-12-20 18:11:32 +0100

    Update Reconnaissance_Formes.h
    Final

--- Reconnaissance_Formes.h
+++ Reconnaissance_Formes.h
@@ -15,53 +15,54 @@
 
 #include "myBmpGris.h"
 
-#define N_ordre 10
+
 
 
 // 1.A
 //free la Matrix
-void freeVM(int N, double** VM);
+void freeVM(int ordre, double** VM);//DEBUGGED
 
 //CrÈation de la matrice Vandermonde pour les puissances d
-double** Vandermonde(int dim, int ordre, double xmoy);
+double** VMpuissance(int dim, int ordre, double xmoy);//DEBUGGED
 
 //Moments GÈomÈtriques
-double Mgeo(BmpImg bmpImg, int p, int q);
+double Mgeo(BmpImg bmpImg, int p, int q);//DEBUGGED
 
 //1.B
 //Moments CentrÈs et normÈs
-double Mcentre(BmpImg bmpImg, int p, int q);
+double Mcentre(BmpImg bmpImg, int p, int q,int beta);//DEBUGGED
 
 // Creer matrice Vandermonde de moments centre
-double** creerMCentree(BmpImg bmpImg,int N);
+double** MatMCentree(BmpImg bmpImg,int N,int beta);//DEBUGGED
 
 //1.C
 //Calcul de Cpq
-double Cpq(int p,int q);
+double Cpq(int p,int q);//DEBUGGED
 
-double** creerCpq(int N);
+double** MatCpq(int ordre);//DEBUGGED
 
 //Moments de Legendre
 //Matrice triangulaire des coefficients de Legendre
 //Renvoie la matrice triangulaire, en fonction de l'ordre NLeg dÈfini au dÈbut du main
-double** coeffLeg (int Nleg);
-
-//Moments de Legendre
-double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre);
-
-//matrice de moments de Legendre
-double** creerMlegendre(int N, double** Cpq, double** coeff, double** Mcentre);
+double** MatCoeffLeg (int ordre);//DEBUGGED
 
 //Construction des polynÙmes de Legendre
 
-double polyLeg(int x, int n, double** coeff);
+double polyLeg(int x, int n, double** coeff);//DEBUGGED
+//Moments de Legendre
+double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre);//DEBUGGED
+
+//matrice de moments de Legendre
+double** MatMlegendre(int ordre, double** Cpq, double** coeff, double** Mcentre);//DEBUGGED
+
+
 //1.D
 //Reconstruction de l'image ‡ partir des moments de Legendre
 
-double ImageMomLeg(int N, int x, int y,double** Mlegendre,double** coeff);
+double pixelReconstruit(int ordre, int x, int y,double** Mlegendre,double** coeff);
 
-double** imageReconstruite(int N, BmpImg bmpimg,double** Mlegendre, double** coeff);
+double** imageReconstruite(int ordre, BmpImg bmpImg,double** Mlegendre, double** coeff);
 
-double distanceEuclidienne(int N, double** Mlegendre1, double** Mlegendre2);
+double distanceEuclidienne(int ordre, double** Mlegendre1, double** Mlegendre2);//DEBUGGED
 
 #endif // RECONNAISSANCE_FORMES_H_INCLUDED
