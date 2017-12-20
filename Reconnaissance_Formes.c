aa4a59b28d23a0dc1372d15b8047edc259461ca8
ilyas Aroui <arouiilyes@gmail.com>
2017-12-20 18:11:00 +0100

    Update Reconnaissance_Formes.c
    Final

--- Reconnaissance_Formes.c
+++ Reconnaissance_Formes.c
@@ -2,59 +2,62 @@
 
 // 1.A
 //free la Matrix
-void freeVM(int N, double** VM)
+void freeVM(int ordre, double** VM) //DEBUGGED
 {
-    int j;
     // Liberation
-    for( j = 0; j <= N; j++ )
-        free( VM[j] );
-    free( VM );
+    if( VM != NULL ){
+        int i;
+        for( i = 0; i <= ordre; i++ )
+            free( VM[i] );
+        free( VM );
+    }
+    VM = NULL;
+    printf("\nImage detruite");
 }
 
 
 //Création de la matrice Vandermonde pour les puissances d
-
-double** Vandermonde(int dim, int ordre, double xmoy){
+double** VMpuissance(int dim, int puissance, double xmoy)//DEBUGGED
+{
 
     int j;
     int x;
-    double** Vdm;
+    double** VM;
 
-    Vdm = malloc(dim * sizeof(double*));
-    
+    VM = malloc(dim * sizeof(double*));
+
     for( j = 0 ; j < dim ; j++){
-      Vdm[j] = calloc(ordre+1,sizeof(double));
+      VM[j] = calloc(puissance+1,sizeof(double));
     }
 
-    if(Vdm == NULL){printf("Erreur allocation");}
+    if(VM == NULL){printf("Erreur allocation");}
 
     for( x = 0 ; x < dim ; x++ ){
-        for( j = 0 ; j <= ordre ; j++){
-            Vdm[x][j] = pow((x-xmoy),j);
+        for( j = 0 ; j <= puissance ; j++){
+            VM[x][j] = pow((x-xmoy),j);
         }
     }
-  return Vdm;
+  return VM;
 }
 
 
 //Moments Géométriques
-double Mgeo(BmpImg bmpImg, int p, int q)
+double Mgeo(BmpImg bmpImg, int p, int q)//DEBUGGED
 {
     unsigned char** image = bmpImg.img;
-    int dimX, dimY; int x, y;
+    unsigned int dimX, dimY; int x, y;
     double moment=0.0;
 
     dimX = bmpImg.dimX;
     dimY = bmpImg.dimY;
 
-    double ** VdmX = Vandermonde(dimX, p, 0); //pas centré donc xb = 0
-    double ** VdmY = Vandermonde(dimY, q, 0); //pas centré donc yb = 0
+    double ** VdmX = VMpuissance(dimX, p, 0); //pas centré donc xb = 0
+    double ** VdmY = VMpuissance(dimY, q, 0); //pas centré donc yb = 0
 
     for (x = 0; x < dimX; x++){
         for (y = 0; y < dimY; y++){
-            
             if (image[x][y] != 0) //pour tout charactère différent de zéro, il faut le transformer en 1
-                moment += VdmX[x][p]*VdmY[y][q]; //no need to multiply by image[x][y] as it's a 1
+                moment += VdmX[x][p]*VdmY[y][q]; //image[x][y]=1
         }
     }
     return moment;
@@ -64,13 +67,13 @@ double Mgeo(BmpImg bmpImg, int p, int q)
 //1.B
 
 //Moments Centrés et normés
-double Mcentre(BmpImg bmpImg, int p, int q){
+double Mcentre(BmpImg bmpImg, int p, int q, int beta)//DEBUGGED
+{
 
-    int dimX, dimY;
-
+    unsigned int dimX, dimY;
+    int x, y;
     unsigned char** image = bmpImg.img;
     double somme=0.0;
-    int x, y;
     dimX = bmpImg.dimX;
     dimY = bmpImg.dimY;
 
@@ -78,35 +81,35 @@ double Mcentre(BmpImg bmpImg, int p, int q){
     double xb = Mgeo(bmpImg, 1, 0)/ohm;
     double yb = Mgeo(bmpImg, 0, 1)/ohm;
 
-    double ** VdmX = Vandermonde(dimX, p, xb);
-    double ** VdmY = Vandermonde(dimY, q, yb);
+    double ** VdmX = VMpuissance(dimX, p, xb);
+    double ** VdmY = VMpuissance(dimY, q, yb);
 
     for(x=0; x<dimX; x++){
         for(y=0; y<dimY; y++){
-            
+
             if(image[x][y]!=0)
-                somme += VdmX[x][p]*VdmY[y][q];//no need to multiply by image[x][y] as it's a 1
+                somme += (VdmX[x][p]*VdmY[y][q])/pow(beta*ohm,((double)(p+q+2.))/2.);//image[x][y]=1
 
         }
     }
-    return somme/(float)(pow(ohm,(float)(p+q+2)/2));
+    return somme;
 }
 
 // Creer matrice Vandermonde de moments centre
 
-double** creerMCentree(BmpImg bmpImg, int N)
+double** MatMCentree(BmpImg bmpImg, int ordre, int beta)//DEBUGGED
 {
     double** VM;
     int j;
-    VM= malloc( (N+1) * sizeof(double*) );
-    for( j = N+1; j >0; j-- )
-        VM[N+1-j] = calloc(j, sizeof(double));
+    VM= malloc( (ordre+1) * sizeof(double*) );
+    for( j = ordre+1; j >0; j-- )
+        VM[ordre+1-j] = calloc(j, sizeof(double));
 
     int p,q;
-    for(p=0;p<=N;p++){
-        for(q=0;q<=N-p;q++){
-    
-            VM[p][q]= Mcentre(bmpImg,p,q);
+    for(p=0; p <= ordre; p++){
+        for(q=0; q <= ordre-p; q++){
+
+            VM[p][q]= Mcentre(bmpImg, p, q, beta);
         }
     }
     return VM;
@@ -116,25 +119,25 @@ double** creerMCentree(BmpImg bmpImg, int N)
 
 //Calcul de Cpq
 
-double Cpq(int p, int q)
+double Cpq(int p, int q)//DEBUGGED
 {
-    double result=(2*p+1)*(2*q+1)/4;
+    double result=(2*p+1)*(2*q+1)/4.;
     return result ;
 }
 
 
-double** creerCpq(int N)
+double** MatCpq(int ordre)//DEBUGGED
 {
     double** VM;
     int j;
-    VM= malloc( (N+1) * sizeof(double*) );
-    for( j = N+1; j >0; j-- )
-        VM[N+1-j] = calloc(j, sizeof(double));
+    VM = malloc( (ordre+1) * sizeof(double*) );
+    for( j = ordre+1; j >0; j-- )
+        VM[ordre+1-j] = calloc(j, sizeof(double));
 
     int p,q;
-    for(p=0;p<=N;p++){
-        for(q=0;q<=N-p;q++){
-            
+    for(p=0; p <= ordre; p++){
+        for(q=0; q <= ordre-p; q++){
+
             VM[p][q]= Cpq(p, q);
         }
     }
@@ -146,7 +149,8 @@ double** creerCpq(int N)
 //Matrice triangulaire des coefficients de Legendre
 //Renvoie la matrice triangulaire, en fonction de l'ordre NLeg défini au début du main
 
-double** coeffLeg (int Nleg) {
+double** MatCoeffLeg (int ordre) //DEBUGGED
+{
 
     //N = taille de la matrice
     int i, j;
@@ -156,15 +160,15 @@ double** coeffLeg (int Nleg) {
     double ** coeff; //matrice triangulaire coeff Legendre
 
     // Allocation dynamique
-    coeff = malloc( (Nleg+1) * sizeof(double*) ); // de la colonne for( int i = 0; i < N; i++ )
-    for( int j = 0; j <= Nleg; j++ )
+    coeff = malloc( (ordre+1) * sizeof(double*) ); // de la colonne for( int i = 0; i < N; i++ )
+    for(j = 0; j <= ordre; j++ )
         coeff[j] = calloc( j+1, sizeof(double) ); // Tableau 1D de  → i+1 elements
 
     //Conditions initiales
 
     coeff[0][0] = 1.0; coeff[1][0] = 0.0; coeff[1][1]= 1.0;
 
-    for(j = 2; j<= Nleg; j++){ //Entre la 2e ligne et la Nème ligne
+    for(j = 2; j<= ordre; j++){ //Entre la 2e ligne et la Nème ligne
 
         for(i=0; i<= n+1; i++){ //Parcours de a entre 0 et n+1
 
@@ -184,25 +188,34 @@ double** coeffLeg (int Nleg) {
         }
         n++; //Incrémentation de n
     }
-    /*/ Affichage: attention aux tailles des tableaux
-    for( j = 0; j <= NLeg; j++ ) {
-        printf( "N = %d : ", j);
-        for( i = 0; i <= j; i++ )
-            printf( "%lf  ", coeff[j][i] ); printf( "\n" );
-    }*/
+     //Affichage: attention aux tailles des tableaux
+//    for( j = 0; j <= ordre; j++ ) {
+//        printf( "N = %d : ", j);
+//        for( i = 0; i <= j; i++ )
+//            printf( "%lf  ", coeff[j][i] ); printf( "\n" );
+//    }
 
     return coeff;
 }
 
+//Construction des polynômes de Legendre
 
+double polyLeg(int x, int n, double** coeff)//DEBUGGED
+{
+    int i; double sum = 0;
+    for(i=0; i<=n; i++)
+        sum += coeff[n][i]*pow(x, i);
+
+    return sum;
+}
 //Moments de Legendre
 
-double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre)
+double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre)//DEBUGGED
 {
     int i,j;
-    double result=0;
-    for(i=0;i<p;i++){
-        for(j=0;j<q;j++){
+    double result=0.0;
+    for(i=0;i<=p;i++){
+        for(j=0;j<=q;j++){
             result+= coeff[p][i]*coeff[q][j]*Mcentre[i][j];
         }
     }
@@ -210,86 +223,76 @@ double Mlegendre(int p, int q, double Cpq, double** coeff, double** Mcentre)
 }
 
 
-double** creerMlegendre(int N, double** Cpq, double** coeff, double** Mcentre)
+double** MatMlegendre(int ordre, double** Cpq, double** coeff, double** Mcentre)//DEBUGGED
 {
     double** VM;
     int j;
-    VM= malloc( (N+1) * sizeof(double*) );
-    for( j = N+1; j >0; j-- )
-        VM[N+1-j] = calloc(j, sizeof(double));
+    VM= malloc((ordre+1) * sizeof(double*) );
+    for( j = ordre+1; j >0; j-- )
+        VM[ordre+1-j] = calloc(j, sizeof(double));
 
     int p,q;
-    for(p=0;p<N+1;p++){
-        for(q=0;q<N+1-p;q++){
-            
+    for(p=0;p<=ordre;p++){
+        for(q=0;q<=ordre-p;q++){
+
             VM[p][q]= Mlegendre(p,q,Cpq[p][q],coeff,Mcentre);
         }
     }
     return VM;
 }
 
-//Construction des polynômes de Legendre
 
-double polyLeg(int x, int n, double** coeff)
-{
-    int i; double sum = 0;
-    for(i=0; i<=n; i++)
-        sum += coeff[n][i]*pow(x, i);
-    
-    return sum;
-}
 
 
 //1.D
 //Reconstruction de l'image à partir des moments de Legendre
 
-double ImageMomLeg(int N, int x, int y, double** Mlegendre, double** coeff)
+double pixelReconstruit(int ordre, int x, int y, double** Mlegendre, double** coeff)
 {
     int p, q;
     double pixel = 0.0;
-    
-    for (p=0; p <= N; p++){
-        for (q = 0; q <= N-p; q++){
 
-            pixel += (Mlegendre[p][q])*polyLeg(x,N,coeff)*polyLeg(y,N,coeff);
+    for (p=0; p <= ordre; p++){
+        for (q = 0; q <= ordre-p; q++){
+
+            pixel += (Mlegendre[p][q])*polyLeg(x,ordre,coeff)*polyLeg(y,ordre,coeff);
         }
     }
     return pixel;
 }
 
 
-double** imageReconstruite(int N, BmpImg bmpimg, double** Mlegendre, double** coeff)
+double** imageReconstruite(int ordre, BmpImg bmpImg, double** Mlegendre, double** coeff)
 {
     int i,x,y;
-    int dimX=bmpimg.dimX;
-    int dimY=bmpimg.dimY;
-    
+    unsigned int dimX=bmpImg.dimX;
+    unsigned int dimY=bmpImg.dimY;
+
     double** image;
     image= malloc(dimX*sizeof(double*));
-    
+
     for(i=0;i<dimX;i++)
-    
         image[i]=calloc(dimY, sizeof(double));
-    
-    
+
+
     for(x=0;x<dimX;x++){
         for(y=0;y<dimY;y++){
-            
-            image[x][y] = ImageMomLeg(N, x, y, Mlegendre, coeff);
+
+            image[x][y] = pixelReconstruit(ordre, x, y, Mlegendre, coeff);
         }
     }
     return image;
 }
 
 
-double distanceEuclidienne(int N, double** Mlegendre1, double** Mlegendre2)
+double distanceEuclidienne(int ordre, double** Mlegendre1, double** Mlegendre2)//DEBUGGED
 {
     int p,q;
     double somme = 0.0;
-    
-    for(p=0;p<=N;p++){
-        for(q=0;q <= N-p;q++){
-    
+
+    for(p=0;p<=ordre;p++){
+        for(q=0;q <= ordre-p;q++){
+
             somme += (Mlegendre1[p][q] - Mlegendre2[p][q]) * (Mlegendre1[p][q] - Mlegendre2[p][q]);
         }
     }
