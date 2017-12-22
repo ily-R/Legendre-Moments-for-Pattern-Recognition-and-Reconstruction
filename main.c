46cb94c9eab8b0cb04bf86dbd3ae269812d41d60
ilyas Aroui <arouiilyes@gmail.com>
2017-12-22 12:24:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 
 #include "myBmpGris.h"
 #include "Reconnaissance_Formes.h"
-#define ordre 45
-#define size 8
+#define ordre 25
+#define size 8 //la taille de notre base de données pour un ordre choisi
+#define true 1
+#define false 0
 int main()
 {
 
+// Si vous voulez recrire la base de données pour un autre ordre decommentez ce qui est en dessous.
 
 //ecrireMlegendre("..\\lady.bmp","..\\lady.txt",ordre,2);
 //ecrireMlegendre("..\\barre.bmp","..\\barre.txt",ordre,2);
@@ -17,6 +20,7 @@ int main()
 //ecrireMlegendre("..\\snow2.bmp","..\\snow2.txt",ordre,2);
 //ecrireMlegendre("..\\stop.bmp","..\\stop.txt",ordre,1);
 //ecrireMlegendre("..\\cercle.bmp","..\\cercle.txt",ordre,1);
+
 
     int i,j;
     double *** a= (double ***)malloc(size*sizeof(double**));
@@ -36,8 +40,8 @@ int main()
     a[6]=lireMlegendre("..\\snow2.txt",ordre);
     a[7]=lireMlegendre("..\\stop.txt",ordre);
 
-    comparaisonImages("stopbis.bmp",ordre,a,1,size);
-//double** image= imageReconstruite(ordre,bmpImg,MatLegendre,coeff);
+    comparaisonImages("lady2",ordre,a,2,size,true);
+
     for(i=0;i<size;i++)
     {
       freeVM(ordre,a[i]);
